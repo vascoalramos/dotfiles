62d6e15830005884b8d10e3e5d7c9a5d82612305
Vasco Ramos <vascoalramos@outlook.pt>
2020-06-07 17:11:00 +0100

    Add gpg key tutorial

--- setup.sh
+++ setup.sh
@@ -12,13 +12,16 @@ printf "\n${WHITE}==============================================================
 SETTING UP YOUR COMPUTER!
 ==============================================================================${NORMAL}\n"
 
-eval "sh scripts/vscode.sh"
+# eval "sh scripts/vscode.sh"
 
 # Follow the instructions on GitHub to generate a new SSH key and add it to your Github account.
 printf "\n${WHITE}==============================================================================
 ${GREEN}All set!
-${WHITE}Make sure to follow the instructions on GitHub to generate a new SSH key and then add the key to your GitHub account.
+${WHITE}Make sure to follow the instructions on GitHub to:
+- generate new SSH and GPG keys
+- add the keys to your GitHub account
 ==============================================================================${NORMAL}\n"
 
 eval "xdg-open https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/#generating-a-new-ssh-key > /dev/null"
 eval "xdg-open https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account > /dev/null"
+eval "xdg-open https://help.github.com/en/github/authenticating-to-github/managing-commit-signature-verification > /dev/null"
