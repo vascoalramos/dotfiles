e7f320e6f3296f4295cc76349d60932e12263c2e
Vasco Ramos <vascoalramos@outlook.pt>
2020-06-07 15:46:24 +0100

    Add vscode extesions and settings

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotfiles
+
 Scripts to simplify setting up new machines
+
+## Useful Repositories
+
+-   Joao Marques: [machine-setup](https://github.com/joao-p-marques/machine-setup)
+-   Brittany Chiang: [dotfiles](https://github.com/bchiang7/dotfiles)
+-   Zell Liew: [dotfiles](https://github.com/zellwk/dotfiles)
+-   Mathias Bynens: [dotfiles](https://github.com/mathiasbynens/dotfiles)
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+WHITE="\033[37m"
+NORMAL="\033[0;39m"
+
+printf "\n${WHITE}==============================================================================
+SETTING UP YOUR COMPUTER!
+==============================================================================${NORMAL}\n"
+
+# eval "sh scripts/vscode.sh"
+
+# Follow the instructions on GitHub to generate a new SSH key and add it to your Github account.
+printf "\n${WHITE}==============================================================================
+${GREEN}All set!
+${WHITE}Make sure to follow the instructions on GitHub to generate a new SSH key and then add the key to your GitHub account.
+==============================================================================${NORMAL}\n"
+eval "xdg-open https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/#generating-a-new-ssh-key > /dev/null"
+eval "xdg-open https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account > /dev/null"
