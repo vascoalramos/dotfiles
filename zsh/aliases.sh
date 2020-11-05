f29e4cbb45812131e4a736d5efc093ef40bab84a
Vasco Ramos <vascoalramos@outlook.pt>
2020-11-05 11:12:59 +0000

    Update git configs
    - Add new git aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,4 +1,4 @@
 alias zshconfig="vim ~/.zshrc"
 alias zshsource="source ~/.zshrc"
-alias open="xdg-open"
+alias explorer="xdg-open"
 alias su-update="sudo apt update; sudo apt upgrade -y; sudo snap refresh"
