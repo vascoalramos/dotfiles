5618b7031e7275c9b4e5db8a86ea820c7b33ea59
Vasco Ramos <vascoalramos@outlook.pt>
2022-09-10 22:56:16 +0100

    Add macos setup

---
+++ macos/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+## Install Homebrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## Install brew bundle command
+brew tap Homebrew/bundle
+
+## Install apps, packages and dependencies
+brew bundle
+
+## Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## Symlink .zshrc
+ln -Fs ~/Projects/dotfiles/macos/.zshrc ~/.zshrc
+
+## Symlink .finicky.js
+ln -s ~/dotfiles/macos/finicky/.finicky.js ~/.finicky.js
+
+## Symlink .gitconfig
+ln -Fs ~/Projects/dotfiles/git/.gitconfig ~/.gitconfig
+
+## Activate Oh My Zsh custom configuration
+source ~/.zshrc
